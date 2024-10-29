1f611d45747df8e087ce437443f1c7a2cb64639e
Julien Windows <juleray@msn.com>
2024-10-29 15:34:27 +0100

    Get datas with a button, dev ok.

--- BasicCRUD/ViewModels/MainViewModel.cs
+++ BasicCRUD/ViewModels/MainViewModel.cs
@@ -1,22 +1,65 @@
 ﻿using BasicCRUD.Contexts;
 using BasicCRUD.Models;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BasicCRUD.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel: INotifyPropertyChanged
     {
         private GamerContext GamerContext;
-        public List<Gamer> Gamers { get; set; }
+
+        private List<Gamer> _gamers;
+        public List<Gamer> Gamers
+        {
+            get { return _gamers; }
+            set
+            {
+                _gamers = value;
+                OnPropertyChanged(nameof(Gamers));
+            }
+        } 
+
+        // button commands //
+
+        private RelayCommand _getGamersCommand;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+
+        public RelayCommand GetGamersCommand
+        {
+            get
+            {
+                return _getGamersCommand ?? (_getGamersCommand = new RelayCommand(GetGamers));
+            }
+        }
 
         public MainViewModel()
         {
-            GamerContext = new GamerContext();
-            Gamers = new List<Gamer>(GamerContext.Gamers.ToList());
+            //GamerContext = new GamerContext();
+            //Gamers = new List<Gamer>(GamerContext.Gamers.ToList());
+        }
+
+        public void GetGamers() {
+
+            using (var context = new GamerContext())
+            {
+                Gamers = new List<Gamer>(context.Gamers.ToList());
+            }
+
         }
     }
 }
